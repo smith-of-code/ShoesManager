9316a41a7633c1ff67863c7c1a3db64d13144a90
andr <goodmena@yandex.ru>
2022-05-01 11:53:40 +0300

    add signout button

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -4,6 +4,7 @@
     <router-link class="mainmenu__item" to="/card">Карточка обуви</router-link>
     <router-link class="mainmenu__item" to="/list">Моя коллекция</router-link>
     <router-link class="mainmenu__item" to="/adviser">Рекомендации</router-link>
+      <a href="/auth/signout">Выход</a>
   </div>
   <router-view />
 </template>
